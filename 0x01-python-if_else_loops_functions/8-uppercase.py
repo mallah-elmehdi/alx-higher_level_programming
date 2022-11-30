14dd2012a8bea777cb117d8b613987ade4f58083
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-30 13:20:02 +0100

    uppercase new logic

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord(c) in range(97, 122)
+    return ord(c) in range(97, 123)
 
 
 def uppercase(str):
-    for i in range(len(str)):
-        if (islower(str[i])):
-            print("{:c}".format(ord(str[i]) - 32),
-                  end="" if i + 1 < len(str) else "\n")
+    upper_str = ''
+    for c in str:
+        if (islower(c)):
+            upper_str += chr(ord(c) - 32)
         else:
-            print(str[i], end="" if i + 1 < len(str) else "\n")
+            upper_str += c
+    print("{:s}".format(upper_str))
