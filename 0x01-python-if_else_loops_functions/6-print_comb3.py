71c8774829b9f4e84c33b80120a7eb345bc5333a
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-30 03:49:03 +0100

    print combo 3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if j != i:
+            print("{:d}{:d}".format(i, j), end="\n" if (
+                j == 8 and i == 9)else ", ")
+print("89")
