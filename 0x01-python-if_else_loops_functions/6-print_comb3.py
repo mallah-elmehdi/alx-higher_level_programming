c689d587232668e629bbb3e33a8eee24456a99a6
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-30 03:51:21 +0100

    update the correct output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
+    for j in range(i, 10):
         if j != i:
             print("{:d}{:d}".format(i, j), end="\n" if (
                 j == 8 and i == 9)else ", ")
-print("89")
