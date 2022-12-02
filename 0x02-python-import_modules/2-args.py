8c500dc79e623b3144403c613d4435aa8337546a
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-02 01:26:05 +0100

    update the logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     arglen = len(argv) - 1
     if arglen == 0:
         print("0 arguments.")
+    elif arglen == 1:
+        print("1 argument:")
     else:
-        print("{} argument".format(arglen), end=":\n" if arglen == 1 else "s:\n")
-        for i in range(1, arglen + 1):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(arglen))
+    for i in range(1, arglen + 1):
+        print("{}: {}".format(i, argv[i]))
