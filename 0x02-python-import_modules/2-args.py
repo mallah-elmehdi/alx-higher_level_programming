e7c5e5ca27d6ae1d7663cc639d36f91a082e199e
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-02 01:18:44 +0100

    change the logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     from sys import argv
 
     arglen = len(argv) - 1
-    print("{} argument".format(arglen), end=".\n" if arglen == 1 else "s:\n")
-    for i in range(1, arglen + 1):
-        print("{}: {}".format(i, argv[i]))
+    if arglen == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument".format(arglen), end=":\n" if arglen == 1 else "s:\n")
+        for i in range(1, arglen + 1):
+            print("{}: {}".format(i, argv[i]))
