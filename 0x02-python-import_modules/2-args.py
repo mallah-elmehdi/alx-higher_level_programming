52023cbcb13a2dbb969dc247561ee7d2e66183ed
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-02 01:13:41 +0100

    update the outpute

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
 
     arglen = len(argv) - 1
-    print(f"{arglen} argument", end=".\n" if arglen == 0 else "s:\n")
+    print("{} argument".format(arglen), end=".\n" if arglen == 1 else "s:\n")
     for i in range(1, arglen + 1):
-        print(f"{i}: {argv[i]}")
+        print("{}: {}".format(i, argv[i]))
