0de8009e6770f8a45788a83e3151ee3d3b852728
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-09 01:25:37 +0100

    remove a    condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,10 +4,9 @@ def new_in_list(my_list, idx, element):
 
     if idx < 0 or idx >= len(my_list):
         return my_list
-    if new_list:
-        for i in range(len(my_list)):
-            if i == idx:
-                new_list[i] == element
-            else:
-                new_list[i] == my_list[i]
+    for i in range(len(my_list)):
+        if i == idx:
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
     return new_list
