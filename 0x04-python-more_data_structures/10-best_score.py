317616fd5e7acff42a8829f06a11f4acef5204e6
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-10 03:32:45 +0100

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
-    return sorted(a_dictionary.items(), key=lambda item: item[1], reverse=True)[0][0]
+    return sorted(a_dictionary.items(), key=lambda i: i[1], reverse=True)[0][0]
