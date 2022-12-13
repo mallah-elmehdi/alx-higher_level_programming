4a9bc3911dbc76c623da23f293514aec95d87252
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-13 14:01:21 +0100

    styling

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    if isinstance(a_dictionary, dict):
-        key_value_pair = list(filter(lambda e: e[1] == value, a_dictionary.items()))
+def complex_delete(a_dict, value):
+    if isinstance(a_dict, dict):
+        key_value_pair = list(filter(lambda e: e[1] == value, a_dict.items()))
         for key in key_value_pair:
-            del a_dictionary[key[0]]
-        return dict(a_dictionary)
+            del a_dict[key[0]]
+        return dict(a_dict)
