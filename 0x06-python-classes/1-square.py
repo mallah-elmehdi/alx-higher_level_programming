c02fde7e0bb6ff8567d01b0936975ab09a649089
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-21 10:39:02 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module documentation: Square class"""
+
+
+class Square:
+    """Square: with private attribute"""
+
+    def __init__(self, size):
+        self.__size = size
+
+    pass
