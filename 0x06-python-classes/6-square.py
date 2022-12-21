666a2049debcdc88e6f1f2bc54b63b7c7f26f56f
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-21 11:30:58 +0100

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,8 @@ class Square:
     @position.setter
     def position(self, value):
         if (
-            type(value[0]) is not int
+            len(value) is not 2
+            or type(value[0]) is not int
             or type(value[1]) is not int
             or value[0] < 0
             or value[1] < 0
