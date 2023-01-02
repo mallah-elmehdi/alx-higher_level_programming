fb69bdb488569bba36d62c95f66336ad5c595130
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-02 17:12:50 +0100

    update condition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """width setter"""
-        if isinstance(value, int) == False:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -38,7 +38,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """height setter"""
-        if isinstance(value, int) == False:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
