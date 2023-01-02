45c54552bb79a7735d37d6894c2510172b29fee4
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-02 15:30:14 +0100

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module documentation: Rectangle"""
+
+
+class Rectangle:
+    """empty class Rectangle"""
+
+    pass
+
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+# x08. Python - More Classes and Objects
