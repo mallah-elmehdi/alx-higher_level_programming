e581ddc3018a9c45f7b774a0e518e8e73978fcc8
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-03 15:18:07 +0100

    N queens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def err(msg):
+    print(msg)
+    exit(1)
+
+
+def main():
+
+    if len(sys.argv) != 2:
+        err("Usage: nqueens N")
+    if not sys.argv[1].isdigit():
+        err("N must be a number")
+    n = int(sys.argv[1])
+    if n < 4:
+        err("N must be at least 4")
+
+
+if __name__ == "__main__":
+    main()
