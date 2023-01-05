93d9bfa0cb5cce93524676a215ab440cb1c6b7ef
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-05 15:56:21 +0100

    Divide a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module function:
+    * matrix_divided(matrix, div):
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Given a matrix and divider, return division of all elements of the matrix.
+
+    Params:
+        -> matrix : list of lists of integers or floats
+        -> div : int
+
+    Return: int
+    """
+
+    if not (
+        isinstance(matrix, list)
+        and all(map(lambda row: isinstance(row, list), matrix))
+        and all(
+            map(
+                lambda row: map(
+                    lambda item: isinstance(item, int) or isinstance(item, float), row
+                ),
+                matrix,
+            )
+        )
+    ):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(matrix) and not all(map(lambda row: len(row) == len(matrix[0]), matrix)):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not (isinstance(div, int) or isinstance(div, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            new_matrix[i].append(round(matrix[i][j] / float(div), 2))
+
+    return new_matrix
