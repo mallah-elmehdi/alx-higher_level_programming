0b3d41e7aacba1900ec5eebc690e0fd66efaa1fd
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-05 19:00:06 +0100

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module function:
+    * def text_indentation(text)
+"""
+
+
+def text_indentation(text):
+    """
+    Given two string, prints a text with 2 new lines after each of these characters: '.', '?' and ':'
+
+    Params:
+        -> text : string
+
+    Return: void
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ['?', ".", ":"]:
+        text = text.replace(delim, f"{delim}\n\n")
+
+    return '\n\n'.join(map(lambda line: line.strip(), text.split("\n\n")))
+
+
+print(text_indentation())
