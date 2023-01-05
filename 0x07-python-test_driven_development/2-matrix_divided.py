444413801569b6751c249508637eb520bb1e309f
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-05 16:19:55 +0100

    getting better

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,27 +15,31 @@ def matrix_divided(matrix, div):
 
     Return: int
     """
-    
-    new_matrix = []
 
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
+    if not (
+        isinstance(matrix, list)
+        and all(map(lambda row: isinstance(row, list), matrix))
+        and all(
+            map(
+                lambda row: all(map(
+                    lambda item: isinstance(
+                        item, int) or isinstance(item, float), row
+                )),
+                matrix,
             )
-        for item in row:
-            if not (isinstance(item, int) or isinstance(item, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats"
-                )
-    if len(matrix) and not all(map(lambda row: len(row) == len(matrix[0]), matrix)):
+        )
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(map(lambda row: len(row) == len(matrix[0]), matrix)):
         raise TypeError("Each row of the matrix must have the same size")
     if not (isinstance(div, int) or isinstance(div, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+
     for i in range(len(matrix)):
         new_matrix.append([])
         for j in range(len(matrix[i])):
