fa28dfb6f71361590fb1d09781e7f3852caa9d55
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-11 12:29:00 +0100

    Square #1

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """
 Module function/classes:
-    * class Rectangle
+    * class Square
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
+class Square(Rectangle):
     """
-    class Rectangle that inherits from BaseGeometry
+    class Square that inherits from Rectangle
     """
 
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return self.__size ** 2
