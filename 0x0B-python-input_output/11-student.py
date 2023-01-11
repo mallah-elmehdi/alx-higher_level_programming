fcd67234945290b7b59e1af66fbdc8952f09954b
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-11 19:11:02 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module class:
+    * class Student
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return dict(filter(
+            lambda item: item[0] in attrs, self.__dict__.items()
+        ))
+
+    def reload_from_json(self, json):
+        self.__setattr__(json)
